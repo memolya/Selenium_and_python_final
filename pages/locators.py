6a28e9e869f7a92be46c79ea99da7df50883b5b8
memolya <semenishevaolchik@ya.ru>
2025-11-28 16:25:03 +0300

    Add test_login_page.py

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,10 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
     REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
+
+class ProductPageLocators():
+    BUTTON = (By.CSS_SELECTOR, "#add_to_basket_form > button")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    SUCCESS_MESSAGE_PRODUCT_NAME = (By.CSS_SELECTOR, "#messages .alert-success .alertinner strong")
+    BASKET_TOTAL = (By.CSS_SELECTOR, "#messages .alert-info .alertinner p strong")
---
+++ pages/product_page.py
@@ -0,0 +1,31 @@
+from pages.base_page import BasePage
+from pages.locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_to_cart(self):
+        add_to_cart_button = self.browser.find_element(*ProductPageLocators.BUTTON)
+        add_to_cart_button.click()
+
+    def should_solve_promo_quiz(self):
+        """Решает промо-квиз (alert с задачкой)."""
+        self.solve_quiz_and_get_code()
+
+    def should_be_product_name_in_success_message(self):
+        """Название товара в сообщении должно совпадать с реальным товаром."""
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        message_product_name = self.browser.find_element(
+            *ProductPageLocators.SUCCESS_MESSAGE_PRODUCT_NAME
+        ).text
+        assert product_name == message_product_name, (
+            f"Имя товара в сообщении не совпадает: "
+            f"ожидали '{product_name}', а в сообщении '{message_product_name}'"
+        )
+
+    def should_basket_total_be_equal_to_product_price(self):
+        """Стоимость корзины должна совпадать с ценой товара."""
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        basket_total = self.browser.find_element(*ProductPageLocators.BASKET_TOTAL).text
+        assert product_price == basket_total, (
+            f"Сумма в корзине '{basket_total}' не равна цене товара '{product_price}'"
+        )
